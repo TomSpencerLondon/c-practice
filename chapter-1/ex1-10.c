db649f5835bb8af7e114244e7f89a7ea12dc1167
Tom <thomas.spencer@codurance.com>
2020-11-11 09:17:26 +0000

    Add ex1 corrections

--- chapter-1/ex1-10.c
+++ chapter-1/ex1-10.c
@@ -5,18 +5,14 @@ int main()
 	char c;
 	
 	while((c = getchar()) != EOF){
-		if (c == '^'){
-			printf("Hello");
+		if (c == '\t'){
+			printf("\\t");
+		}else if (c =='\b'){
+			printf("\\b");
+		}else if (c == '\\'){
+			printf("\\\\");
+		}else {
+			putchar(c);
 		}
-		
-		// if (c == '\t'){
-// 			printf("\\t");
-// 		}else if (c =='\r'){
-// 			printf("hello");
-// 		}else if (c == '\\'){
-// 			printf("\\\\");
-// 		}else {
-// 			putchar(c);
-// 		}
 	}
 }
