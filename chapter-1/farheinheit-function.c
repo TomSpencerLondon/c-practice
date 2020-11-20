324e1545105a9e3d7a321b478eb2d09c29438c07
Tom <thomas.spencer@codurance.com>
2020-11-20 12:51:59 +0000

    Add farhenheit function

---
+++ chapter-1/farheinheit-function.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+print Fahrenheit-Celsius table
+for farh = 0, 20, ..., 300
+*/
+
+#define LOWER 0.0
+#define UPPER 300.0
+#define STEP 20.0
+
+float celsius(float farh);
+
+int main() {
+	float fahr;
+	
+	printf("\t  %c\t   %c\n", 'F', 'C');
+	for (fahr = UPPER; fahr >= LOWER; fahr -= STEP) {
+		printf("\t%3.0f\t%6.1f\n", fahr, celsius(fahr));
+	}
+	
+	return 0;
+}
+
+float celsius(float fahr){
+	float celsius = (5.0 / 9.0) * (fahr - 32.0);
+	return celsius;
+}
--- chapter-1/histogram-characters.c
+++ chapter-1/histogram-characters.c
@@ -3,16 +3,17 @@
 #define NUMBER_OF_LETTERS 26;
 
 int main() {
+	int letters;
+	letters = NUMBER_OF_LETTERS;
 	
 	int i;
-	int characterFrequencies[26];
+	int characterFrequencies[letters];
 	int character;
 	
 	
 	char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 	
-	int letters;
-	letters = NUMBER_OF_LETTERS;
+
 	
 	for (i = 0; i < letters; i++){
 		characterFrequencies[i] = 0;
@@ -29,7 +30,38 @@ int main() {
 		}
 	}
 	
+	
+	int maxOccurrence = 0;
+	
 	for (i = 0; i < letters; i++){
-		printf("%d", characterFrequencies[i]);
+		if (characterFrequencies[i] > maxOccurrence){
+			maxOccurrence = characterFrequencies[i];
+		}
 	}
+	
+	int el;
+	
+	for (i = 0; i <= maxOccurrence; i++){
+		for (el = 0; el < letters; el++){
+			if ((characterFrequencies[el] == maxOccurrence - i) && (characterFrequencies[el] > 0)){
+				printf("- ");
+			}else if ((characterFrequencies[el] > maxOccurrence - i) && characterFrequencies[el] > 0) {
+				printf("| ");
+			}else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	
+	for (el = 0; el < letters; el++){
+		if (characterFrequencies[el] > 0){
+			printf("- ");
+		}else {
+			printf("  ");
+		}
+	}
+	printf("\n");
+	
+	printf("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z");
 }
