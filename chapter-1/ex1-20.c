b884e9e1da3c883bf15a67e038c8564036105d32
Tom <thomas.spencer@codurance.com>
2020-12-14 09:39:12 +0000

    Add

--- chapter-1/ex1-20.c
+++ chapter-1/ex1-20.c
@@ -5,7 +5,26 @@
 // 2. n = spaces between each tab
 // the quick	  brown fox
 // 12345678901234
-		
+#include <stdio.h>
+#define TAB_SPACES 8
+                     
 int main() {
+	int c;
+	int position = 0;
 	
+	while((c = getchar()) != EOF){
+		if (c == '\t'){
+			int numberOfSpaces = TAB_SPACES - position % TAB_SPACES;
+			for (int i = 0; i < numberOfSpaces; i++){
+				putchar(' ');
+			}
+			position += numberOfSpaces;
+		}else if(c == '\n') {
+			putchar(c);
+			position = 0;
+		}else {
+			putchar(c);
+			position++;
+		}
+	}
 }
